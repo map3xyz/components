943cb575d8f805d440d39235f52f9b791488a96f
plondon <philip.n.london@gmail.com>
2022-11-02 09:43:32 -0400

    Add pill stories

--- src/components/Pill/Pill.stories.tsx
+++ src/components/Pill/Pill.stories.tsx
@@ -7,5 +7,20 @@ export default {
 export const CoinStory = () => (
   <div className="flex w-full flex-col items-start">
     <Pill color="green">Hello World</Pill>
+    <Pill className="mt-2" color="green" dot>
+      Hello World
+    </Pill>
+    <Pill
+      className="mt-2"
+      color="green"
+      dot
+      icon={<i className={`fa fa-check-circle`} />}
+      role="button"
+    >
+      Hello World
+    </Pill>
+    <Pill className="mt-2" color="gray" dot role="button">
+      Hello World
+    </Pill>
   </div>
 );
--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -1,27 +1,64 @@
 import React, { PropsWithChildren } from 'react';
 
 const Pill: React.FC<PropsWithChildren<Props>> = (props) => {
+  let text = '';
+  let bg = '';
+  let border = '';
+  let dotBg = '';
+  switch (props.color) {
+    case 'green':
+      text = 'text-green-500';
+      border = 'border-green-800';
+      dotBg = 'bg-green-500';
+      bg = 'bg-green-900/50';
+      break;
+    case 'red':
+      text = 'text-red-500';
+      border = 'border-red-800';
+      dotBg = 'bg-red-500';
+      bg = 'bg-red-900/50';
+      break;
+    case 'yellow':
+      text = 'text-yellow-500';
+      border = 'border-yellow-800';
+      dotBg = 'bg-yellow-500';
+      bg = 'bg-yellow-900/50';
+      break;
+    case 'blue':
+      text = 'text-blue-500';
+      border = 'border-blue-800';
+      dotBg = 'bg-blue-500';
+      bg = 'bg-blue-900/50';
+      break;
+    default:
+      text = 'text-gray-400';
+      border = 'border-gray-600';
+      dotBg = 'bg-gray-400';
+      bg = 'bg-gray-600/50';
+      break;
+  }
+
   return (
     <div
-      className={`flex items-center gap-1 rounded-full border bg-opacity-50 py-1 px-2 text-xs font-semibold ${
-        props.color === 'green'
-          ? 'border-green-800 bg-green-900 text-green-500'
-          : props.color === 'red'
-          ? 'border-red-800 bg-red-900 text-red-500'
-          : props.color === 'blue'
-          ? 'border-blue-800 bg-blue-900 text-blue-500'
-          : props.color === 'gray'
-          ? 'border-gray-800 bg-gray-900 text-gray-500'
-          : ''
-      }`}
+      {...props}
+      className={`flex items-center gap-1 rounded-full border py-1 px-2 text-xs font-semibold ${text} ${border} ${bg} ${props.className}`}
     >
+      {props.dot ? (
+        <span className={`h-2 w-2 rounded-full ${dotBg}`}></span>
+      ) : null}
+      {props.icon ? (
+        <span className={`${text} !bg-transparent`}>{props.icon}</span>
+      ) : null}
       {props.children}
     </div>
   );
 };
 
-type Props = {
-  color: 'green' | 'gray' | 'red' | 'blue';
-};
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  color: 'green' | 'gray' | 'red' | 'blue' | 'yellow';
+  dot?: boolean;
+  icon?: React.ReactElement;
+}
 
 export default Pill;
