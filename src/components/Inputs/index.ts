01051c426f09189138640fb3938de332f2856915
plondon <philip.n.london@gmail.com>
2023-02-25 09:17:57 -0500

    Add radio to exports

--- src/components/Inputs/index.ts
+++ src/components/Inputs/index.ts
@@ -1,4 +1,5 @@
 export { default as Input } from './Input';
+export { default as Radio } from './Radio';
 export { default as ReadOnlyText } from './ReadOnlyText';
 export { default as Select } from './Select';
 export { default as Textarea } from './Textarea';
