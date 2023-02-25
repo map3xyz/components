a9661a429b88c50089c2e16824b8dc8a51a75e1b
plondon <philip.n.london@gmail.com>
2023-02-25 09:31:46 -0500

    Change radio label style

--- src/components/Inputs/Radio.stories.tsx
+++ src/components/Inputs/Radio.stories.tsx
@@ -9,6 +9,8 @@ export default {
 
 export const All = () => (
   <div className="map3 flex max-w-sm flex-col gap-2">
-    <Radio label="Radio" value="radio" />
+    <Radio label="Radio 1" name="radio-1" value="radio1" />
+    <Radio label="Radio 2" name="radio-1" value="radio2" />
+    <Radio label="Radio 3" name="radio-1" value="radio3" />
   </div>
 );
